c613ebca9a74daf1081b53a8aede74ef74ed5a6d
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-04-08 10:40:32 +0530

    Update cloning.sh

--- cloning.sh
+++ cloning.sh
@@ -1,4 +1,4 @@
-cd pe
+cd evo
 git clone https://github.com/BCA-RMIT/android_device_asus_X01AD.git -b lineage-19.0-back device/asus/X01AD
 git clone https://github.com/BCA-RMIT/vendor_asus_X01AD.git -b lineage-18.1 vendor/asus/X01AD
-git clone https://github.com/BCA-RMIT/android_kernel_asus_X01AD.git -b lineage-18.1 kernel/asus/X01AD
+git clone https://github.com/LineageOS/android_kernel_asus_X01AD.git -b lineage-18.1 kernel/asus/X01AD
