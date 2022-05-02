84ac23b85f33df77f6189fe3727913a235525e7e
Subhendu-droid <55965922+Subhendu-droid@users.noreply.github.com>
2022-05-03 01:00:17 +0530

    Update synching.sh

--- synching.sh
+++ synching.sh
@@ -2,5 +2,5 @@ git clone https://github.com/akhilnarang/scripts
 sudo bash scripts/setup/android_build_env.sh
 mkdir evo
 cd evo
-repo init -u https://github.com/Evolution-X/manifest -b snow
+repo init -u https://github.com/PixelExperience/manifest -b eleven-plus
 repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
